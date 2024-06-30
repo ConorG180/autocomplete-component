8e473ec7267cad695f98b812841d2241567ee9f5
Conor Ganly <conor.ganly@yahoo.com>
2024-06-30 17:46:38 +0100

    Improve backend searching functionality and update test

--- backend/main.py
+++ backend/main.py
@@ -22,15 +22,37 @@ with open("data.json") as f:
 
 
 @app.get("/search")
+@app.get("/search")
 def search(query: str = Query()):
     results = []
     for artist in data:
+        artist_matched = False
+        
+        # Check for artist match
         if query.lower() in artist["name"].lower():
             results.append(artist)
+            artist_matched = True
+
         for album in artist.get("albums", []):
+            album_matched = False
+
+            # Check for album matches
             if query.lower() in album["title"].lower():
                 results.append(album)
+                album_matched = True
+
+            # Include songs if album  matched
+            if album_matched:
+                results.extend(album["songs"])
+
+            # Include album and songs if artist is matched
+            if artist_matched:
+                results.append(album)
+                results.extend(album["songs"])
+
+            # Check for song match
             for song in album.get("songs", []):
-                if query.lower() in song["title"].lower():
+                if query.lower() in song["title"].lower() and not artist_matched and not album_matched:
                     results.append(song)
+    
     return results
--- backend/test_main.py
+++ backend/test_main.py
@@ -7,4 +7,11 @@ def test_search():
     response = client.get("/search?query=RaDioHeAd") # Spelt with caps to ensure result is not case sensitive
     assert response.status_code == 200
     results = response.json()
+    # Artist check
     assert any("Radiohead" in result["name"] for result in results)
+    # Album check
+    assert any("The King of Limbs" in result.get("title", "") for result in results)
+    assert any("OK Computer" in result.get("title", "") for result in results)
+    # Song check
+    assert any("Bloom" in result.get("title", "") for result in results)
+    assert any("Airbag" in result.get("title", "") for result in results)
