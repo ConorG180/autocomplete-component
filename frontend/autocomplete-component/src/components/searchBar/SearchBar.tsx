70b878fe8b1e461a5cd7fd46ee073b4c1019e359
Conor Ganly <conor.ganly@yahoo.com>
2024-06-30 15:56:05 +0100

    Add action button for searchbar and styling

--- frontend/autocomplete-component/src/components/searchBar/SearchBar.tsx
+++ frontend/autocomplete-component/src/components/searchBar/SearchBar.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Suggestions from "../suggestions/Suggestions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./SearchBar.css";
 
 const searchBar: React.FC = () => {
@@ -9,16 +11,25 @@ const searchBar: React.FC = () => {
         setQuery(e.target.value);
     };
 
+    const handleSearch = () => {
+        console.log(query);
+    };
+
     return (
         <div className="search-bar-container">
             <h1>Search for your favourite artists, albums and songs!</h1>
-            <input
-                type="text"
-                className="search-input"
-                placeholder="Search..."
-                value={query}
-                onChange={handleInputChange}
-            />
+            <div className="search-input-wrapper">
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search..."
+                    value={query}
+                    onChange={handleInputChange}
+                />
+                <button className="search-button" onClick={handleSearch}>
+                    <FontAwesomeIcon icon={faSearch} size="xl" />
+                </button>
+            </div>
             {query && <Suggestions query={query} />}
         </div>
     );
