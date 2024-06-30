214a3ded1543fba00db2943f3a1857a4e7454c3b
Conor Ganly <conor.ganly@yahoo.com>
2024-06-30 17:47:09 +0100

    Fix searching with whitespace bug

--- frontend/autocomplete-component/src/requests/requests.ts
+++ frontend/autocomplete-component/src/requests/requests.ts
@@ -4,7 +4,7 @@ const BASE_URL = "http://localhost:8000";
 
 export const getSuggestions = async (query: string) => {
     try {
-        const response = await axios.get(`${BASE_URL}/search?query=${query}`);
+        const response = await axios.get(`${BASE_URL}/search?query=${query.trim()}`);
         return response.data;
     } catch (error) {
         console.error(error);
