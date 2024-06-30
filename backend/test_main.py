e41b3ace3103a45dd9540109e32087ef18a7cbb6
Conor Ganly <conor.ganly@yahoo.com>
2024-06-30 16:09:51 +0100

    Create test for search endpoint

---
+++ backend/test_main.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_search():
+    response = client.get("/search?query=RaDioHeAd") # Spelt with caps to ensure result is not case sensitive
+    assert response.status_code == 200
+    results = response.json()
+    assert any("Radihead" in result["name"] for result in results)
